984ed901a47de1f80214d8d6580594904547a54b
Bret Petersen <bretpetersen82@gmail.com>
2023-11-28 15:49:20 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Box from "./Box";
+import BoxArrayOfDefaultObjects from "./BoxArrayOfDefaultObjects";
+// import "./styles.css";
 
-function App() {
+const App = () => {
+
+  // define data
+  let boxArrayOfObjects = localStorage.getItem('boxArrayOfObjectsUnique') ? JSON.parse(localStorage.getItem('boxArrayOfObjectsUnique')) : BoxArrayOfDefaultObjects;
+  // console.log(boxArrayOfObjects);
+  // get data
+  async function readData() {
+    boxArrayOfObjects = localStorage.getItem('boxArrayOfObjectsUnique') ? JSON.parse(localStorage.getItem('boxArrayOfObjectsUnique')) : boxArrayOfObjects;
+  }
+  // set data
+  readData().then(() => {
+    localStorage.setItem('boxArrayOfObjectsUnique', JSON.stringify(boxArrayOfObjects));
+  })
+
+  const [dragId, setDragId] = useState();
+  const [boxes, setBoxes] = useState(boxArrayOfObjects);
+
+  const handleDrag = (ev) => {
+    setDragId(ev.currentTarget.id);
+  };
+
+  const handleDrop = (ev) => {
+    const dragBox = boxes.find((box) => box.id === dragId);
+    const dropBox = boxes.find((box) => box.id === ev.currentTarget.id);
+
+    const dragBoxOrder = dragBox.order;
+    const dropBoxOrder = dropBox.order;
+
+    const newBoxState = boxes.map((box) => {
+      if (box.id === dragId) {
+        box.order = dropBoxOrder;
+      }
+      if (box.id === ev.currentTarget.id) {
+        box.order = dragBoxOrder;
+      }
+      return box;
+    });
+    boxArrayOfObjects = newBoxState;
+    localStorage.setItem('boxArrayOfObjectsUnique', JSON.stringify(boxArrayOfObjects));
+    setBoxes(newBoxState);
+
+    console.log('newBoxState: ' + JSON.stringify(newBoxState));
+    // console.log('boxArrayOfObjects: ' + JSON.stringify(boxArrayOfObjects));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {boxes
+        .sort((a, b) => a.order - b.order)
+        .map((box) => (
+          <Box
+            key={box.id}
+            boxColor={box.color}
+            boxNumber={box.id}
+            handleDrag={handleDrag}
+            handleDrop={handleDrop}
+          />
+        ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/BoxArrayOfDefaultObjects.js
@@ -0,0 +1,19 @@
+const BoxArrayOfDefaultObjects = [
+  {
+    id: "Box-1",
+    color: "red",
+    order: 1
+  },
+  {
+    id: "Box-2",
+    color: "green",
+    order: 2
+  },
+  {
+    id: "Box-3",
+    color: "blue",
+    order: 3
+  }
+];
+
+export default BoxArrayOfDefaultObjects;
